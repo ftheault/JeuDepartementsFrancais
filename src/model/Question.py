5d8d40886c2904d249707fd605188c2d661ec819
theault <florian.theault@ecole.ensicaen.fr>
2022-08-30 19:42:30 +0200

    hot fix

--- src/model/Question.py
+++ src/model/Question.py
@@ -3,18 +3,19 @@ import random
 from src.model.QuestionType import QuestionType
 
 
+def generate_random_question_type():
+    return QuestionType(random.randint(1, 3))
+
+
 class Question:
     def __init__(self):
-        self.question_type = ""
+        self.question_type = generate_random_question_type()
         self.main_question = ""
 
-    def generate_random_question_type(self):
-         return QuestionType(random.randint(1, 3))
-
-    def generate_random_question(self, question_type):
-        if question_type == QuestionType.DEPARTMENT:
+    def generate_random_question(self,):
+        if self.question_type == QuestionType.DEPARTMENT:
             return ""
-        elif question_type == QuestionType.DEPARTMENT_NUMBER:
+        elif self.question_type == QuestionType.DEPARTMENT_NUMBER:
             return ""
         else:
             return ""
