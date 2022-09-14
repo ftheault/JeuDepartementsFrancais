2d030a52c5f2118b8f2cc2f353550345be10c793
theault <florian.theault@ecole.ensicaen.fr>
2022-09-14 20:07:47 +0200

    hot fix

--- src/model/Result.py
+++ src/model/Result.py
@@ -1,6 +1,6 @@
 class CheckAnswer:
-    def __int__(self, department, department_number, chief_town):
-        self.department = department
+    def __int__(self, department_name, department_number, chief_town):
+        self.department_name = department_name
         self.department_number = department_number
         self.chief_town = chief_town
 
