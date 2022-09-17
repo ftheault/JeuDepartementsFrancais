d8d5fb16d30f1e5af54037630c8e435d477a2045
theault <florian.theault@ecole.ensicaen.fr>
2022-09-17 12:01:58 +0200

    checking answer method

--- src/model/Result.py
+++ src/model/Result.py
@@ -1,8 +1,10 @@
 class Answer:
-    def __int__(self, department_name, department_number, chief_town):
-        self.department_name = department_name
-        self.department_number = department_number
-        self.chief_town = chief_town
+    def __int__(self, question_department, user_response_department):
+        self.user_response_department = user_response_department
+        self.question_department = question_department
 
     def checking(self):
-        return True
+        if self.user_response_department == self.question_department:
+            return True
+        else:
+            return False
